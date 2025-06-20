880479cb251a87ac3936164768cb240b8ad2397e
ByteJoseph <josephnjoseph43@gmail.com>
2025-06-20 16:44:58 +0530

    added server

---
+++ docs/functions/handler.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async function(event, context) {
+  const userAgent = event.headers['user-agent'] || "";
+  const isCurl = /curl|wget|httpie/i.test(userAgent);
+
+  const filePath = isCurl ? "installer.sh" : "index.html";
+  const contentType = isCurl ? "text/x-sh" : "text/html";
+
+  try {
+    const fileData = fs.readFileSync(path.resolve(__dirname, "../", filePath), "utf8");
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": contentType,
+      },
+      body: fileData
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Internal Server Error"
+    };
+  }
+};
+
