5de7b1497744f5f0b7d6f479325507086a61a02e
ByteJoseph <josephnjoseph43@gmail.com>
2025-06-20 17:02:07 +0530

    edit handler

--- functions/handler.js
+++ functions/handler.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.handler = async function(event, context) {
-  const userAgent = event.headers['user-agent'] || "";
+exports.handler = async function (event, context) {
+  const userAgent = event.headers["user-agent"] || "";
   const isCurl = /curl|wget|httpie/i.test(userAgent);
 
-  const filePath = isCurl ? "installer.sh" : "index.html";
+  const fileName = isCurl ? "installer.sh" : "index.html";
+  const filePath = path.resolve(__dirname, "../docs", fileName);
   const contentType = isCurl ? "text/x-sh" : "text/html";
 
   try {
-    const fileData = fs.readFileSync(path.resolve(__dirname,"../docs", filePath), "utf8");
+    const fileData = fs.readFileSync(filePath, "utf8");
     return {
       statusCode: 200,
       headers: {
         "Content-Type": contentType,
       },
-      body: fileData
+      body: fileData,
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: "Internal Server Error"
+      body: "Internal Server Error",
     };
   }
 };
