99dd06006b089d0919b2c5dfff73439c8ebd4954
ByteJoseph <josephnjoseph43@gmail.com>
2025-06-20 16:04:18 +0530

    created installer script

---
+++ installer/installer.sh
@@ -0,0 +1,13 @@
+curl -sSL https://raw.githubusercontent.com/ByteJoseph/noexec/refs/heads/main/src/noexec -o noexec
+file="noexec"
+dir_name=$(dirname "$(which bash)")
+if [ -f "$file" ]; then
+  
+  cp "$file" "$dir_name/$file"
+  chmod +x "$dir_name/$file"
+  echo "noexec Installed Successfully"
+  rm "noexec"
+  echo ""
+  echo "Start using now"
+  noexec
+fi
